917aaa769d00924d3e1a8309e7861d4501c669e7
Noobswag2 <kpate237@charlotte.edu>
2025-04-15 13:56:48 -0400

    Add user authentication system (signup, login, logout)

--- resurgejustice_project/urls.py
+++ resurgejustice_project/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from django.urls import path
-from core import views  
+from django.urls import path, include
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('team/', views.team, name='team'),
-    path('contact/', views.contact, name='contact'), 
+    path('contact/', views.contact, name='contact'),
     path('profile/', views.profile, name='profile'),
     path('jobs/', views.job_listings, name='job_listings'),
     path('mentor/', views.mentor, name='mentor'),
     path('company/', views.company, name='company'),
+    path('accounts/', include('accounts.urls')),
 ]
